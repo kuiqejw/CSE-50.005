dcd56af60c682307e57f590b7e595d63ffc528df
kuiqejw <lauraong1@yahoo.com.sg>
2018-04-13 04:22:48 +0800

    CP2
    Could be shortened further. Please check and modify accordingly

---
+++ ProgAss2/prog2/ClientCP2.java
@@ -0,0 +1,279 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog2;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class ClientCP2 {
+
+    private static final String serverAddress = "localhost";
+    private static int port = 1234;
+    private static final String uploadFilePath = "C:\\Users\\ongajong\\Documents\\50.005\\Prog2\\src\\prog2\\smallFile.txt";
+    private static String filename = "small.txt";
+    private static final String CACertFile = "C:\\Users\\ongajong\\Documents\\50.005\\Prog2\\src\\prog2\\CA.crt.txt";
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            filename = args[0];
+        }
+
+        if (args.length > 1) {
+            filename = args[1];
+        }
+
+        if (args.length > 2) {
+            port = Integer.parseInt(args[2]);
+        }
+
+        int numBytes = 0;
+
+        Socket clientSocket = null;
+
+        DataOutputStream toServer = null;
+        DataInputStream fromServer = null;
+
+        FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedFileInputStream = null;
+
+        long timeStarted = System.nanoTime();
+
+        try {
+
+            System.out.println("Establishing connection to server...");
+
+            // Connect to server and get the input and output streams
+            clientSocket = new Socket(serverAddress, port);
+            // channels for sending and receiving bytes
+            OutputStream byteOut = clientSocket.getOutputStream();
+            InputStream byteIn = clientSocket.getInputStream();
+
+            PrintWriter stringOut = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader stringIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//Wait for server to respond
+            stringOut.println("CLIENT>> Hello SecStore, please prove your identity!");
+            stringOut.flush();
+            System.out.println("Sent to server: Hello SecStore, please prove your identity!");
+// wait for server to respond
+            String firstResponse = stringIn.readLine();
+            System.out.println(firstResponse);
+
+            // send a nonce
+            byte[] nonce = generateNonce();
+            if (firstResponse.contains("this is SecStore")) {
+                stringOut.println(Integer.toString(nonce.length));
+                byteOut.write(nonce);
+                byteOut.flush();
+                System.out.println("Sent to server a fresh nonce");
+            }
+
+            //System.out.println(nonce);
+            // retrieve encrypted nonce from server
+            String encryptedNonceLength = stringIn.readLine();
+            System.out.println("encrypted Nonce length: " + encryptedNonceLength);
+            byte[] encryptedNonce = new byte[Integer.parseInt(encryptedNonceLength)];
+            ServerWithoutSecurity.readByte(encryptedNonce, byteIn);
+            System.out.println("Received encrypted nonce from server");
+
+            // ask for certificate
+            stringOut.println("CLIENT>> Give me your certificate signed by CA");
+            stringOut.flush();
+            System.out.println("Sent to server: Give me your certificate signed by CA");
+
+            // extract public key from CA certificate
+            InputStream fis = new FileInputStream(CACertFile);
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate caCert = (X509Certificate) certificateFactory.generateCertificate(fis);
+            PublicKey caPublicKey = caCert.getPublicKey();
+            System.out.println("CA public key extracted");
+
+            // retrieve signed certificate from server
+            String certByteArrayLength = stringIn.readLine();
+            stringOut.println("CLIENT>> Ready to get certificate");
+            stringOut.flush();
+            byte[] certByteArray = new byte[Integer.parseInt(certByteArrayLength)];
+            ServerWithoutSecurity.readByte(certByteArray, byteIn);
+            System.out.println("Received certificate from server");
+
+            // verifying signed certificate from server using CA public key
+            System.out.println("Verifying certificate from server");
+            InputStream certInputStream = new ByteArrayInputStream(certByteArray);
+            X509Certificate signedCertificate = (X509Certificate) certificateFactory.generateCertificate(certInputStream);
+
+            signedCertificate.checkValidity();
+            signedCertificate.verify(caPublicKey);
+            System.out.println("Signed certificate validity checked and verified");
+
+            // extract public key from server's signed certificate
+            PublicKey serverPublicKey = signedCertificate.getPublicKey();
+
+            // create cipher object and initialize it as decrypt mode, using PUBLIC key.
+            Cipher cipherDecrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipherDecrypt.init(Cipher.DECRYPT_MODE, serverPublicKey);
+
+            // decrypt nonce
+            byte[] decryptedNonce = cipherDecrypt.doFinal(encryptedNonce);
+
+            // handles connection after decrypting nonce.
+            if (Arrays.equals(nonce, decryptedNonce)) {
+                System.out.println("Server's identity verified");
+                stringOut.println("CLIENT>> Ready to upload file!");
+                stringOut.flush();
+            } else {
+                System.out.println("Identity verification unsuccessful, closing all connections");
+                stringOut.println("CLIENT>> Bye!");
+                byteOut.close();
+                byteIn.close();
+                stringOut.close();
+                stringIn.close();
+                clientSocket.close();
+            }
+
+            System.out.println("Sending file...");
+            // initial time mark
+            Long startTime = System.currentTimeMillis();
+            
+        // create cipher object and initialize is as encrypt mode, use PUBLIC key.
+        Cipher rsaCipherEncrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        rsaCipherEncrypt.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+
+        // generate AES key
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey aesKey = keyGen.generateKey();
+
+        // convert secret key to byte array
+        byte[] aesKeyBytes = aesKey.getEncoded();
+
+        // encrypt AES key
+        byte[] encryptedAESKeyBytes = rsaCipherEncrypt.doFinal(aesKeyBytes);
+
+        // create cipher object for file encryption
+        Cipher aesEnCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        aesEnCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+        // get bytes of file needed for transfer
+        File file = new File(uploadFilePath);
+        byte[] fileBytes = new byte[(int) file.length()];
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        bis.read(fileBytes, 0, fileBytes.length);
+
+        // encrypt file with AES key
+        byte[] encryptedFileBytes = aesEnCipher.doFinal(fileBytes);
+
+        // send encrypted AES session key
+        stringOut.println(encryptedAESKeyBytes.length);
+        stringOut.flush();
+        System.out.println(stringIn.readLine());
+        byteOut.write(encryptedAESKeyBytes, 0, encryptedAESKeyBytes.length);
+        byteOut.flush();
+        System.out.println("Sent to server encrypted session key");
+
+
+        // upload encrypted file to server
+        stringOut.println(filename);
+        stringOut.println(encryptedFileBytes.length);
+        stringOut.flush();
+        System.out.println(stringIn.readLine());
+        byteOut.write(encryptedFileBytes, 0, encryptedFileBytes.length);
+        byteOut.flush();
+        System.out.println("Sent to server encrypted file");
+
+            // create cipher object and initialize is as encrypt mode, us
+            
+//            e PUBLIC key.
+//            Cipher rsaCipherEncrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+//            rsaCipherEncrypt.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+//
+//            // generate AES key
+//            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+//            keyGen.init(128);
+//            SecretKey aesKey = keyGen.generateKey();
+//            // convert secret key to byte array
+//            byte[] aesKeyBytes = aesKey.getEncoded();
+//
+//            // encrypt AES key
+//            byte[] encryptAESKeyBytes = rsaCipherEncrypt.doFinal(aesKeyBytes);
+//
+//            // create cipher object for file encryption
+//            Cipher aesEnCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+//            aesEnCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+//
+//            // get bytes of file needed for transfer
+//            File file = new File(uploadFilePath);
+//            byte[] fileBytes = new byte[(int) file.length()];
+//            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+//            bis.read(fileBytes, 0, fileBytes.length);
+//
+//            // encrypt file with AES key
+//            byte[] encryptedFileBytes = aesEnCipher.doFinal(fileBytes);
+//
+//            // send encrypted AES session key
+//            stringOut.println(encryptAESKeyBytes.length);
+//            stringOut.flush();
+//            System.out.println(stringIn.readLine());
+//            byteOut.write(encryptAESKeyBytes, 0, encryptAESKeyBytes.length);
+//            byteOut.flush();
+//            System.out.println("Sent to server encrypted session key");
+//
+//            // start file transfer
+//            byte[] encryptedFile = ServerWithoutSecurity.encrypt(uploadFilePath, rsaCipherEncrypt);
+//
+//            // send encrypted file
+//            stringOut.println(filename);
+//            stringOut.println(encryptedFile.length);
+//            System.out.println(stringIn.readLine());
+//            byteOut.write(encryptedFile, 0, encryptedFile.length);
+//            byteOut.flush();
+
+            // confirmation of successful file upload
+            System.out.println(stringIn.readLine());
+
+            Long endTime = System.currentTimeMillis();
+            System.out.println("Uploading time spent is: " + (endTime - startTime) + "ms");
+            byteOut.close();
+            byteIn.close();
+            stringOut.close();
+            stringIn.close();
+            clientSocket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+private static void encrypt2(PrintWriter stringOut, PublicKey serverPublicKey, BufferedReader stringIn, OutputStream byteOut) throws Exception{
+
+}
+    private static byte[] generateNonce() throws NoSuchAlgorithmException {
+        // create secure random number generator
+        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+
+        // get 1024 random bytes
+        byte[] nonce = new byte[64];
+        secureRandom.nextBytes(nonce);
+        return nonce;
+    }
+}
---
+++ ProgAss2/prog2/ServerCP2.java
@@ -0,0 +1,210 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog2;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author ongajong
+ */
+public class ServerCP2 {
+
+    private static final int NTHREADS = 5;
+    private static ExecutorService executorService = Executors.newFixedThreadPool(NTHREADS);
+    private static ServerSocket serverSocket;
+    private static final int port = 1234;
+    private static final String privateKeyFile = "C:\\Users\\ongajong\\Documents\\50.005\\Prog2\\src\\prog2\\privateServer.der";
+    private static final String signedCertificateFile = "C:\\Users\\ongajong\\Documents\\50.005\\Prog2\\src\\prog2\\server.crt";
+
+    public static void main(String[] args) {
+
+        ServerSocket welcomeSocket = null;
+//        DataOutputStream toClient = null;
+//        DataInputStream fromClient = null;
+
+//        FileOutputStream fileOutputStream = null;
+//        BufferedOutputStream bufferedFileOutputStream = null;
+        try {
+            welcomeSocket = new ServerSocket(port);
+
+            while (true) {
+                // then create TCP ClientSocket upon accepting incoming TCP request
+                System.out.println("... expecting connection ...");
+                final Socket connectionSocket = welcomeSocket.accept();
+                System.out.println("... connection established...");
+
+                // create threads to handle multiple client uploads
+                Runnable task = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            OutputStream byteOut = connectionSocket.getOutputStream();
+                            InputStream byteIn = connectionSocket.getInputStream();
+
+                            // channels for sending and receiving plain text
+                            PrintWriter stringOut = new PrintWriter(byteOut, true);
+                            BufferedReader stringIn = new BufferedReader(new InputStreamReader(byteIn));
+                            // wait for client to initiate conversation
+                            System.out.println(stringIn.readLine());
+
+                            // reply to client
+                            stringOut.println("SERVER>> Hello, this is SecStore!");
+                            stringOut.flush();
+                            System.out.println("Sent to client: Hello, this is SecStore!");
+
+                            // retrieve nonce from client
+                            String nonceLength = stringIn.readLine();
+                            byte[] nonce = new byte[Integer.parseInt(nonceLength)];
+                            readByte1(nonce, byteIn);
+                            System.out.println("Received fresh nonce from client");
+
+                            // load private key from .der
+                            Path privateKeyPath = Paths.get(privateKeyFile);
+                            byte[] privateKeyByteArray = Files.readAllBytes(privateKeyPath);
+                            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyByteArray);
+                            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+
+// Create cipher object and initialize is as encrypt mode, use PRIVATE key.
+                            Cipher rsaCipherEncrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                            rsaCipherEncrypt.init(Cipher.ENCRYPT_MODE, privateKey);
+
+                            
+                            // encrypt nonce
+                            byte[] encryptedNonce = rsaCipherEncrypt.doFinal(nonce);
+                            stringOut.println(Integer.toString(encryptedNonce.length));
+                            byteOut.write(encryptedNonce, 0, encryptedNonce.length);
+                            byteOut.flush();
+                            System.out.println("Sent encrypted nonce to Client");
+                            // wait for client response
+                            System.out.println(stringIn.readLine());
+
+                            // send signed certificate
+                            File certificateFile = new File(signedCertificateFile);
+                            byte[] certByteArray = new byte[(int) certificateFile.length()];
+                            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(certificateFile));
+                            bis.read(certByteArray, 0, certByteArray.length);
+
+                            stringOut.println(Integer.toString(certByteArray.length));
+                            System.out.println(stringIn.readLine());
+                            byteOut.write(certByteArray, 0, certByteArray.length);
+                            byteOut.flush();
+                            System.out.println("Sent to client certificate");
+
+                            // receive messages from client
+                            String clientResult = stringIn.readLine();
+                            System.out.println(clientResult);
+                            if (clientResult.contains("Bye!")) {
+                                byteOut.close();
+                                byteIn.close();
+                                stringOut.close();
+                                stringIn.close();
+                                connectionSocket.close();
+                            }
+                            
+                            System.out.println("File Transfer Initiated");
+                            
+                            
+                            String encryptAESBytesLength = stringIn.readLine();
+                            stringOut.println("Server>> Ready to receive encrypted session key");
+                            stringOut.flush();
+
+                            byte[] encryptedAESKeyBytes = new byte[Integer.parseInt(encryptAESBytesLength)];
+                            ServerWithoutSecurity.readByte(encryptedAESKeyBytes, byteIn);
+                            System.out.println("Received session key from client");
+
+                            //get encrypted file from client
+                            String filename = stringIn.readLine();
+                            String encryptFileBytesLength = stringIn.readLine();
+                            stringOut.println("Server>> Ready to receive encrypted file: ");
+                            stringOut.flush();
+
+                            byte[] encryptedFileBytes = new byte[Integer.parseInt(encryptFileBytesLength)];
+                            readByte1(encryptedFileBytes, byteIn);
+                            //received encrypted file from client
+
+                            //Use cipher object, initialize as decrypt mode
+                            Cipher rsaCipherDecrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                            rsaCipherDecrypt.init(Cipher.DECRYPT_MODE, privateKey);
+                            //decrypt to get AES key bytes
+
+                            byte[] aesKeyByte = rsaCipherDecrypt.doFinal(encryptedAESKeyBytes);
+                            System.out.println("AES KEY");
+                            SecretKey aesKey = new SecretKeySpec(aesKeyByte, 0, aesKeyByte.length, "AES");
+
+                            //decryption of file creates cipher object 
+                            Cipher aesCipherDecrypt = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                            aesCipherDecrypt.init(Cipher.DECRYPT_MODE, aesKey);
+
+                            //decrypt AES encrypted File
+                            byte[] fileBytes = aesCipherDecrypt.doFinal(encryptedFileBytes);
+                            System.out.println("File decrypted");
+
+                            // create new file and write to file
+                            FileOutputStream fileOut = new FileOutputStream(filename);
+                            fileOut.write(fileBytes, 0, fileBytes.length);
+                            System.out.println("File registered into system.");
+                            
+                            
+                            // send confirmation of successful upload to client
+                            stringOut.println("SERVER>> Upload file successful!");
+                            stringOut.flush();
+                            
+                            byteOut.close();
+                            System.out.println("");
+                            byteIn.close();
+                            stringOut.close();
+                            stringIn.close();
+                            connectionSocket.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                executorService.execute(task);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+private static void readByte1(byte[] byteArray, InputStream byteIn) throws Exception{
+        int offset = 0;
+        int numRead = 0;
+        while (offset < byteArray.length && (numRead = byteIn.read(byteArray, offset, byteArray.length - offset)) >= 0){
+            offset += numRead;
+        }
+        if (offset < byteArray.length) {
+            System.out.println("File reception incomplete!");
+        }
+}
+    
+}
